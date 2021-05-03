f4d23e47233fc56f95679b2c91571c3247f24555
Nick <nickjbianco25@gmail.com>
2021-05-03 13:11:26 -0400

    Add style to app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ToDoForm from "./components/ToDoForm";
+import ToDoList from "./components/ToDoList";
 import "./App.css";
+import Typography from "@material-ui/core/Typography";
+
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    storageTodos && setTodos(storageTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
   const addToDo = (todo) => {
     setTodos([todo, ...todos]);
   };
 
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+
+        return todo;
+      })
+    );
+  };
+
+  const removeToDo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React To Do</h1>
-        <ToDoForm addToDo={addToDo} />
-      </header>
+      <Typography style={{ padding: 16 }} variant="h1">
+        React To Do
+      </Typography>
+      <ToDoForm addToDo={addToDo} />
+      <ToDoList
+        todos={todos}
+        toggleComplete={toggleComplete}
+        removeToDo={removeToDo}
+      />
     </div>
   );
 };
